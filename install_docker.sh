774408fe3923d8b4e56a1d7a98b0829f61afc95a
Guilherme Chavenco <gchavenco@lliege.com.br>
2023-11-24 16:34:13 -0300

    Install docker

---
+++ install_docker.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+clear
+# Instalando o docker
+echo "Instalando o docker"
+
+# Atualizar a lista de pacotes
+sudo apt update
+
+# Instalar dependências
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Adicionar a chave GPG oficial do Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Adicionar o repositório estável do Docker
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Atualizar a lista de pacotes após adicionar o repositório
+sudo apt update
+
+# Instalar a versão mais recente do Docker
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+# Adicionar o usuário ao grupo "docker" para executar comandos Docker sem sudo
+sudo usermod -aG docker $USER
+
+# Reiniciar o serviço Docker
+sudo systemctl restart docker
+
+# ----- Install docker compose -----
+clear
+echo "Instalando o docker-compose"
+
+# Baixar a versão mais recente do Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Dar permissão de execução ao binário do Docker Compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Criar um link simbólico para o comando "docker-compose"
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# Verificar a instalação
+docker-compose --version
