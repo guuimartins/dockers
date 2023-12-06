eb29f009128d8cd0ca0f68b384aa02ba36eee79e
Guilherme Chavenco <gchavenco@lliege.com.br>
2023-12-06 13:33:09 -0300

    Script para atualizar o docker e docker-compose

--- update_docker.sh
+++ update_docker.sh
@@ -6,20 +6,21 @@ if [ "$EUID" -ne 0 ]
   exit;
 fi
 
+clear
 echo "Atualizando o docker:"
 
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+sudo apt-get update -y
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
 echo "Atualizando o docker compose"
 
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose -y
+sudo chmod +x /usr/local/bin/docker-compose -y
 
 echo
 echo "Docker atualizado."
